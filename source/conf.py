d87303f333875edcff894b36e28fea5d07306456
Jackson Carhart <jcarhart@uchicago.edu>
2016-06-09 09:31:12 -0500

    altered path in conf.py, pointed Sphinx towards source folder

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('C:\Users\Jack\python_projects\Code\documentation_master\ex25\example25'))
+sys.path.insert(0, os.path.abspath('C:\Users\Jack\python_projects\Code\documentation_master\ex25\source\example25'))
 
 
 # -- General configuration ------------------------------------------------
